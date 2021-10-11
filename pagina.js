7ef22813da56ad30d589ce0ae8bb3c5e7a9e8724
robinschijf <robin@muiswerk.nl>
2021-10-11 16:32:41 +0200

    first commit

---
+++ pagina.js
@@ -0,0 +1,13 @@
+function telop(a, b){
+    var result = a + b;
+    return result;
+}
+
+var resulttelop = telop(10, 5);
+//alert(resulttelop)
+
+var pTekst =  document.getElementById('myid').innerText;
+alert(pTekst);
+
+document.getElementById('myid').innerText = "hello world";
+document.getElementById('myid').style.fontSize = '25px';
